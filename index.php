195663903a04e6d463fb53a0ea75e73ba90daff2
babazze <babazze@live.fr>
2019-12-02 16:05:10 +0100

    go

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$km = 1;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex3</title>
+  </head>
+  <body>
+      <p><?= $km; ?></p>
+      <p><?= 2 + $km; ?></p>
+      <p><?= -$km * -125; ?></p>
+  </body>
+</html>
